da012c6262066bb2bb6da2ed6640280854246722
charles rainey <charles.rainey@gmail.com>
2021-04-17 13:01:03 -0400

    updated models

--- models/parks.js
+++ models/parks.js
@@ -1,54 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ParkSchema = new Schema({
-    parkCode: {
-        type: String,
-        trim: true,
-        required: true,
-        validate: [
-            function(input) {
-              return input.length >= 1;
-            },
-            "Title should be longer than 1"
-          ]
-    },
-    location: {
-      type: String,
-      trim: true,
-    },
-    // start: Date,
-    // end: Date,
-    // description: String,
-    // created: {
-    //   type: Date,
-    //   default: Date.now
-    // },
-    // lastUpdate:Date,
-    // user: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "User"
-    // },
-    // guests: [{
-    //   type: Schema.Types.ObjectId,
-    //   ref: "User"
-    // }],
-    // allDay: {
-    //   type: Boolean,
-    //   default: true
-    // } DELETE THIS CODE
+const parkSchema = new Schema({
+  fullName: { type: String, required: true },
+  addresses: { type: String, required: true },
+  description: { type: String, required: true },
+  standardHours: { type: String, required: true },
+  entranceFees: { type: String, required: true },
+  date: { type: Date, default: Date.now }
 });
 
-// // Custom method `lastUpdatedDate`
-// ParkSchema.methods.lastUpdatedDate = function() {
-//   // Set the current user's `lastUpdated` property to the current date/time
-//   this.lastUpdated = Date.now();
-//   // Return this new date
-//   return this.lastUpdated;  
-//DELETE THIS CODE
-
-
-
-const Park = mongoose.model("Park", parksSchema);
+const Park = mongoose.model("Park", parkSchema);
 
 module.exports = Park;
